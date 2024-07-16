41d250255447d00a54a926c28f4184b5567824a0
matheuscascao <matheusfranco23@hotmail.com>
2024-07-16 20:24:23 -0300

    prettier

--- src/interfaces/cliente.interface.ts
+++ src/interfaces/cliente.interface.ts
@@ -1,18 +1,18 @@
 export interface ICliente {
-    id: number
-    name: string
-    cpf: string
-    data_nascimento: string
-    renda_estimada: number
+  id: number;
+  name: string;
+  cpf: string;
+  data_nascimento: string;
+  renda_estimada: number;
 }
 
 export interface IClienteCreate {
-    name: string
-    cpf: string
-    data_nascimento: string
-    renda_estimada: number
+  name: string;
+  cpf: string;
+  data_nascimento: string;
+  renda_estimada: number;
 }
 
 export interface IClienteRepository {
-    create(data: IClienteCreate): Promise<ICliente>
+  create(data: IClienteCreate): Promise<ICliente>;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import fastify, {FastifyInstance} from "fastify";
-import {clienteRoutes} from "./routes/cliente.routes"
-const app: FastifyInstance = fastify({logger: true})
+import fastify, { FastifyInstance } from 'fastify';
+import { clienteRoutes } from './routes/cliente.routes';
+const app: FastifyInstance = fastify({ logger: true });
 
 app.register(clienteRoutes, {
-    prefix: '/clientes'
-})
+  prefix: '/clientes',
+});
 
 app.listen(
-    {
+  {
     port: 3100,
-    },
-    () => console.log('Server is running on port 3100')
-)
+  },
+  () => console.log('Server is running on port 3100')
+);
